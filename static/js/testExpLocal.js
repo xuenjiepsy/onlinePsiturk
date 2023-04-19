316f592a5e0a4d7775e852ac48f6d14e18da6911
xuenjiepsy <103114445+xuenjiepsy@users.noreply.github.com>
2023-04-19 20:28:56 +0800

    n

--- static/js/testExpLocal.js
+++ static/js/testExpLocal.js
@@ -197,11 +197,11 @@ var expEndIntro = {
 
 // timeline.push(introduction);
 // timeline.push(introductionVideo1);
-timeline.push(intro);
 timeline.push({
   type: jsPsychFullscreen,
   fullscreen_mode: true
 });
+timeline.push(intro);
 timeline.push(experiments);
 timeline.push(expEndIntro)
 // if (typeof jsPsych !== "undefined") {
